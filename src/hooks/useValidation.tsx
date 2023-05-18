dbc3c279996ede39d02c8ccc4dd6e38258217f7f
onezeun <onezeun@gmail.com>
2023-05-18 15:57:52 +0900

    feat: 로그인 구현

---
+++ src/hooks/useValidation.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+interface ValidationResult {
+  valid: boolean;
+  message: string;
+}
+
+const UID_CHECK = /^[a-z0-9\.\-_]+@([a-z0-9\-]+\.)+[a-z]{2,6}$/;
+const PW_CHECK = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[A-Z])(?=.*[a-z\d])[A-Za-z][A-Za-z\d]*$/;
+
+const uidValidation = (uid: string): ValidationResult => {
+  if (uid.length === 0) {
+    return { valid: false, message: '이메일을 입력해 주세요' };
+  } else if (!UID_CHECK.test(uid)) {
+    return { valid: false, message: '이메일 형식으로 입력해주세요' };
+  } else return { valid: true, message: '' };
+};
+
+const pwValidation = (pw: string): ValidationResult => {
+  if (pw.length === 0) {
+    return { valid: false, message: '비밀번호를 입력해주세요' };
+  } else if (!PW_CHECK.test(pw)) {
+    return { valid: false, message: '비밀번호 형식으로 입력해주세요' };
+  } else return { valid: true, message: '' };
+};
+
+export default function useValidation() {
+  const [uid, setUid] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [uidErrMsg, setUidErrMsg] = useState<null | string>(null);
+  const [pwdErrMsg, setPwdErrMsg] = useState<null | string>(null);
+
+  const uidChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let value = e.target.value;
+    const uidCheck = uidValidation(value);
+
+    setUid(value);
+    setUidErrMsg(uidCheck.message);
+  };
+
+  const pwChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let value = e.target.value;
+    const pwCheck = pwValidation(value);
+
+    setPassword(value);
+    setPwdErrMsg(pwCheck.message);
+  };
+
+  const user = { uid: uid, password: password };
+  const errMsg = { uid: uidErrMsg, pwd: pwdErrMsg };
+
+  return { user, uidChange, pwChange, errMsg };
+}
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useValidation from 'hooks/useValidation';
 import Layout from 'components/Layout';
-import 'styles/Login.css';
 
 function Login() {
+  const { user, uidChange, pwChange, errMsg } = useValidation();
+  const navigate = useNavigate();
+  const [loginError, setLoginError] = useState(false);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (errMsg.uid === '' && errMsg.pwd === '') {
+      setLoginError(false);
+      localStorage.setItem('user', JSON.stringify(user));
+      navigate('/main');
+    } else setLoginError(true);
+  };
+
   return (
     <Layout>
       <h1>로그인</h1>
 
-      <form name="loginForm" action="" method="post" className="login-form">
+      <form name="loginForm" action="" method="post" className="login-form" onSubmit={handleSubmit}>
         <label htmlFor="uid">아이디</label>
-        <input type="text" name="uid" id="uid"></input>
+        <input type="text" name="uid" id="uid" onChange={uidChange} />
+        {errMsg.uid && <p>{errMsg.uid}</p>}
         <label htmlFor="pwd">비밀번호</label>
-        <input type="password" name="pwd" id="pwd"></input>
+        <input type="password" name="pwd" id="pwd" onChange={pwChange} />
+        {errMsg.pwd && <p>{errMsg.pwd}</p>}
         <button type="submit">로그인</button>
+        {loginError && <p className="error-message">로그인에 실패하였습니다. 다시 시도해주세요.</p>}
       </form>
     </Layout>
   );
