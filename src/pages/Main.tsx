144129e5ec8c3dd4c3ae8c44df5ccb9e1c9c59cf
onezeun <onezeun@gmail.com>
2023-05-18 15:56:37 +0900

    feat: 메인페이지 로그아웃 버튼 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Layout from 'components/Layout';
 
 function Main() {
+  const navigate = useNavigate();
+
+  const onClickHandler = () => {
+    localStorage.removeItem('user');
+    navigate('/login');
+  };
+
   return (
     <Layout>
-      <h1>메인페이지</h1>
+      <div className="main">
+        <h1>메인페이지</h1>
+        <button type="button" onClick={onClickHandler}>
+          로그아웃
+        </button>
+      </div>
     </Layout>
   );
 }
